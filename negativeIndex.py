b8e0dbdd3b44c976463472b2e4536d58863d6545
Sedvin1903 <83063914+Sedvin1903@users.noreply.github.com>
2022-01-18 16:15:54 +0530

    Assignment 1.2 - 3

---
+++ negativeIndex.py
@@ -0,0 +1,9 @@
+"""   Assignment 1.2 - 3 """
+
+List = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+# Positive Index - First to last 
+print("Positive Index:",List[0:])
+
+# Negative Index - Last to first 
+print("Negative Index:",List[-7:])
