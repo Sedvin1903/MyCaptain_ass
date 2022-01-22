acf813fdaf623d3558cce0eaeb997bc16c5937c1
Sedvin1903 <83063914+Sedvin1903@users.noreply.github.com>
2022-01-22 20:23:47 +0530

    Add files via upload

---
+++ Live_Session_assignment/ifElse_impl.py
@@ -0,0 +1,21 @@
+"""   Assignment 1.1 - 2 """
+
+# If Else statement Implementation 
+
+# 2.a - If it snows I will got Else I will Not 
+
+import random as r
+
+snow = r.randint(0,1)
+print("Snow = ",snow)
+if snow == 1 :
+  print("I will go out")
+else:
+  print("I will not go out")
+
+# 2.b - If it rains frogs will dance
+
+rain = 1
+
+if rain :
+  print("Frogs will Dance") 
